56bbb427435bec2a4c7879302806f03dbd59682f
whereugo <elbar.ace@mail.ru>
2023-10-26 17:30:14 +0300

    comment

--- main.js
+++ main.js
@@ -36,17 +36,17 @@
 // (На самом деле, этот вопрос подходит для собеседований, и, говорят, он позволяет отсеивать довольно большое число кандидатов. Поэтому, когда вы решите эту задачу, можете себя похвалить.)
 
 
-for (let i = 1; i < 100; i++) {
-    if (i%3===0 && i%5===0) {
-    console.log('fizzbuzz');
-    }
-        else if (i%3===0){
-            console.log('fizz');
-        }
-        else if(i%5===0) {
-            console.log('buzz');
-        }
-    else {
-        console.log(i)
-    }
-}
+// for (let i = 1; i < 100; i++) {
+//     if (i%3===0 && i%5===0) {
+//     console.log('fizzbuzz');
+//     }
+//         else if (i%3===0){
+//             console.log('fizz');
+//         }
+//         else if(i%5===0) {
+//             console.log('buzz');
+//         }
+//     else {
+//         console.log(i)
+//     }
+// }
